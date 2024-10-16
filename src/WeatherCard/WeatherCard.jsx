57b56bd95b4bf1fc2759c38192d0e3bbc9c4bbc3
Pinar Gulum <pinargulum@hotmail.com>
2024-10-16 02:43:24 -0400

    update

--- src/WeatherCard/WeatherCard.jsx
+++ src/WeatherCard/WeatherCard.jsx
@@ -1,11 +1,31 @@
 import "../WeatherCard/WeatherCard.css";
-import sunny from "../../src/assets/sunny.png";
-function WeatherCard ({ weatherData }) {
-    return(
-        <section className="weather__card">
-        <p className="weather__card-temp">{weatherData.temp.F} F</p>
-        <img src={sunny} alt="sunny" className="weather__card-image" />
-            </section>
-    )
+import { weatherOptions, defaultWeatherOptions } from "../utils/constant";
+
+function WeatherCard({ weatherData }) {
+  const filteredOptions = weatherOptions.filter((option) => {
+    return (
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condition
+    );
+  });
+  let weatherOption;
+  if (filteredOptions.length === 0) {
+    weatherOption = defaultWeatherOptions[weatherData.isDay ? "day" : "night"];
+  } else {
+    weatherOption = filteredOptions[0];
+  }
+
+  return (
+    <section className="weather__card">
+      <p className="weather__card-temp">{weatherData.temp.F} F</p>
+      <img
+        src={weatherOption?.url}
+        alt={`Card showing  ${weatherOption?.day ? "day" : "night"}time ${
+          weatherOption?.condition
+        }weather`}
+        className="weather__card-image"
+      />
+    </section>
+  );
 }
-export default WeatherCard
+export default WeatherCard;
