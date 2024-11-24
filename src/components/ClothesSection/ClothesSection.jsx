fc695aebb7d4596b41ffc4d49c83a88223eb2aa7
Pinar Gulum <pinargulum@hotmail.com>
2024-11-23 22:52:52 -0500

    update

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -1,12 +1,25 @@
-import "../ClothesSection/ClothesSection.css"
+import "../ClothesSection/ClothesSection.css";
+import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constant";
 
-function ClothsSection () {
-    return(
-        <section className="add__cloths-section">
-            <p className="cloths__add-items">Your items</p>
-            <button className="cloths__add-button" type="button">+ Add new</button>
-
-        </section>
-    )
+function ClothsSection() {
+  return (
+    <div className="your__items">
+      <section className="your__items-section">
+        <p className="cloths__add-items">Your items</p>
+        <button className="cloths__add-button" type="button">
+          + Add new
+        </button>
+      </section>
+      <section className="your__items-card-section">
+       <li className="your__items-card-list">
+        return(
+            
+            {defaultClothingItems}
+        )
+       </li>
+      </section>
+    </div>
+  )
 }
-export default ClothsSection
+export default ClothsSection;
