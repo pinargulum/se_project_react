75c327dbf304c5b3972ff9027452e5b581c51c1c
Pinar Gulum <pinargulum@hotmail.com>
2024-12-07 00:17:38 -0500

    update

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -3,28 +3,25 @@ import ItemCard from "../ItemCard/ItemCard.jsx";
 import { defaultClothingItems } from "../../utils/constant";
 function ClothsSection() {
   return (
-    <div className="cloths__section">
-      <div>
-    <div className="your__items">
+    <div>
+      <div className="your__items">
         <p className="cloths__add-items">Your items</p>
         <button className="cloths__add-button" type="button">
           + Add new
         </button>
-       </div>
-        <ul className="cards__list cards__list-cloths">
-          {defaultClothingItems.map((item) => {
-            return (
-              <ItemCard
-                key={item._id}
-                item={item}
-                //onCardClick={handleCardClick}
-              />
-            );
-          })}
-        </ul>
-        </div>
-        
-  </div>
+      </div>
+      <ul className="cards__list cards__list-cloths">
+        {defaultClothingItems.map((item) => {
+          return (
+            <ItemCard
+              key={item._id}
+              item={item}
+              //onCardClick={handleCardClick}
+            />
+          );
+        })}
+      </ul>
+    </div>
   );
 }
 export default ClothsSection;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,8 +5,13 @@ import SideBar from "../SideBar/SideBar";
 function Profile() {
     return(
         <div className="profile">
+            <section className="profile__sidebar">
             <SideBar />
+            </section>
+            <section className="profile__clothing-items">
             <ClothsSection />
+            </section>
+            
         </div>
     )
 }
