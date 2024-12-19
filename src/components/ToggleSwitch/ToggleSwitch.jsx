6b28ce0a8b4a012fefb27a57090f75ddc78d918d
Pinar Gulum <pinargulum@hotmail.com>
2024-12-19 04:39:50 -0500

    Update ToggleSwitch.jsx

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import "./ToggleSwitch.css"
+import "./ToggleSwitch.css";
 
 import CurrentTemperatureUnitContext from "../../utils/CurrentTemperatureUnitContext.js";
 
