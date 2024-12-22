e3d0cc28674a1f163807c328be6aa7367b2e15c3
Pinar Gulum <pinargulum@hotmail.com>
2024-12-21 21:47:57 -0500

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,7 @@
 import "../Footer/Footer.css";
 function Footer() {
   return (
-  
     <footer className="footer">
-      
       <h3 className="footer__title">Developed by Pinar Gulum</h3>
       <p className="footer__year">{new Date().getFullYear()}</p>
     </footer>
