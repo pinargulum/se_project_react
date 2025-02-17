abf64bde3306437a315ab76335cad46486038122
Pinar Gulum <pinargulum@hotmail.com>
2025-02-17 00:43:20 -0500

    update

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -6,47 +6,41 @@ import ProfileEditModal from "../ProfileEditModal/ProfileEditModal.jsx";
 import { removeToken } from "../../utils/token.js";
 //import { Link } from "react-router-dom";
 
-function SideBar({ isLoggedIn,  handleEditClick }) {
-const currentUser = useContext(CurrentUserContext);
+function SideBar({ isLoggedIn, handleEditClick }) {
+  const currentUser = useContext(CurrentUserContext);
 
   const handleLogout = () => {
-    removeToken();  
-  }
+    removeToken();
+  };
 
   return (
     <div className="profile__sidebar">
       <div className="sidebar__user-container">
-    
-      <img
-        src={currentUser.avatar}
-        alt="profile-picture"
-        className="sidebar__avatar"
-        
-      />
-      <p className="sidebar__username">{currentUser.name}</p>
-     
-      <div className="change__profile">
-     
-      <button
-        onClick={handleEditClick}
-        type="button"
-       className="sidebar__button"
-      >
-      Change profile data
-      </button>
-      
-      <button
-        onClick={handleLogout}
-        type="button"
-       className="sidebar__button"
-      >
-        Sign Out
-      </button>
-      
+        <img
+          src={currentUser.avatar}
+          alt="profile-picture"
+          className="sidebar__avatar"
+        />
+        <p className="sidebar__username">{currentUser.name}</p>
       </div>
+      <div className="sidebar__change-profile">
+        <button
+          onClick={handleEditClick}
+          type="button"
+          className="sidebar__button"
+        >
+          Change profile data
+        </button>
+
+        <button
+          onClick={handleLogout}
+          type="button"
+          className="sidebar__button"
+        >
+          Log out
+        </button>
       </div>
     </div>
-    
   );
 }
 export default SideBar;
