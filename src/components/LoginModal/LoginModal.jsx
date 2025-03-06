399e0f111431e4e454b71ea7fae5cb2a71a93ba0
Pinar Gulum <pinargulum@hotmail.com>
2025-03-06 03:39:57 -0500

    update

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -6,16 +6,11 @@ import { useForm } from "../../utils/hooks/useForm";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 const LoginModal = ({ isOpen, onCloseModal, handleLogin, registerModal }) => {
-  const { values, handleChange, setValues } = useForm({ email: "", password: "" });
+  const { values, handleChange, setValues } = useForm({
+    email: "",
+    password: "",
+  });
 
-
-
-  //function handleEmailChange(e) {
-  //values
-  //}
-  //function handlePasswordChange(e) {
-  //setPassword(e.target.value);
-  //}
   useEffect(() => {
     if (isOpen) {
       setValues("");
@@ -23,7 +18,7 @@ const LoginModal = ({ isOpen, onCloseModal, handleLogin, registerModal }) => {
   }, [isOpen]);
 
   function handleSubmit(evt) {
-   const inputValues = (values.email, values.password)
+    const inputValues = (values.email, values.password);
     evt.preventDefault();
     handleLogin({ inputValues });
   }
@@ -36,23 +31,34 @@ const LoginModal = ({ isOpen, onCloseModal, handleLogin, registerModal }) => {
       onSubmit={handleSubmit}
       modifierClass="login"
     >
-      <label className="modal__label">Email:</label>
-
+      <label
+        className="modal__label"
+        htmlFor="email"
+      >
+        Email:
+      </label>
       <input
         type="email"
         className="login_input"
         placeholder="Email"
         onChange={handleChange}
         value={values.email}
+        required
       />
 
-      <label className="modal__label">Password:</label>
+      <label
+        className="modal__label"
+        htmlFor="email"
+      >
+        Password:
+      </label>
       <input
         type="password"
         onChange={handleChange}
         className="login_input"
         placeholder="Password"
         value={values.password}
+        required
       />
       <button
         onClick={registerModal}
