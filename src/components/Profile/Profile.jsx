3164abef52b3cff0577d8261304a6e42df4b827e
Pinar Gulum <pinargulum@hotmail.com>
2025-03-09 06:14:39 -0400

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -16,24 +16,28 @@ function Profile({
   const currentUser = useContext(CurrentUserContext);
 
   return (
-    <div className="profile">
-      <section className="profile__sidebar">
-        <SideBar
-          handleEditClick={handleEditClick}
-          isLoggedIn={isLoggedIn}
-        />
-      </section>
-      <section className="profile__clothing-items">
-        <ClothesSection
-          isLoggedIn={isLoggedIn}
-          handleCardClick={handleCardClick}
-          clothingItems={clothingItems}
-          handleProfileAddItem={handleProfileAddItem}
-          handleCardLike={handleCardLike}
-          likedItems={likedItems}
-        />
-      </section>
-    </div>
+    <>
+      {currentUser && (
+        <div className="profile">
+          <section className="profile__sidebar">
+            <SideBar
+              handleEditClick={handleEditClick}
+              isLoggedIn={isLoggedIn}
+            />
+          </section>
+          <section className="profile__clothing-items">
+            <ClothesSection
+              isLoggedIn={isLoggedIn}
+              handleCardClick={handleCardClick}
+              clothingItems={clothingItems}
+              handleProfileAddItem={handleProfileAddItem}
+              handleCardLike={handleCardLike}
+              likedItems={likedItems}
+            />
+          </section>
+        </div>
+      )}
+    </>
   );
 }
 export default Profile;
